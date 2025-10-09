deded0ba7750c7de9247a0584aca6035b5d1554a
MOUAD RARHIB <mouad159mouad@gmail.com>
2025-10-09 10:31:29 +0100

    Add logout method and fix cors in app.js

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Frontend URL
+  credentials: true, // Allow cookies
+}));
 app.use(express.json({ limit: '1mb' }));
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
